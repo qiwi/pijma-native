8d4633e3cb862679a92f3b7abf0e2d8b6968d28e
Dima Livshits <dimonka83@gmail.com>
2022-05-27 13:01:47 +0300

    docs: sections from packages

--- packages/web/styleguide.config.js
+++ packages/web/styleguide.config.js
@@ -15,8 +15,24 @@ module.exports = {
     {
       name: 'Pijma',
       content: path.resolve(__dirname, '..', '..', 'README.md'),
-      components: ['../../packages/**/src/**/*.{ts,tsx}'],
+      components: [],
       sectionDepth: Number.MAX_VALUE,
+      sections: fs
+        .readdirSync(path.resolve(__dirname, '..'))
+        .filter((file) => !['native', 'android', 'ios'].includes(file))
+        .filter((file) =>
+          fs
+            .lstatSync(path.resolve(__dirname, '..', file, 'README.md'), {
+              throwIfNoEntry: false,
+            })
+            ?.isFile(),
+        )
+        .map((file) => ({
+          name: file,
+          content: path.resolve(__dirname, '..', file, 'README.md'),
+          components: [`../${file}/src/**/*.{ts,tsx}`],
+          sectionDepth: Number.MAX_VALUE,
+        })),
     },
   ],
   styleguideDir: path.resolve(__dirname, 'lib'),
